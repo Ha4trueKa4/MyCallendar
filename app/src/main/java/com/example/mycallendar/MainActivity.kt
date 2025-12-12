7dcba9915305fff356dde27ad6eb1c2e45f81454
Dean <kelemandean76@gamil.com>
2025-12-12 23:50:20 +0300

    Code refactoring

--- app/src/main/java/com/example/mycallendar/MainActivity.kt
+++ app/src/main/java/com/example/mycallendar/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.mycallendar
 
+import android.R
 import android.app.DatePickerDialog
+import android.os.Build
+import java.time.LocalDate
 import java.text.SimpleDateFormat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,11 +19,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,16 +39,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.integerResource
+import androidx.compose.ui.text.style.TextAlign
 import java.util.Locale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
 import com.example.mycallendar.ui.theme.MyCallendarTheme
 import java.text.DateFormat
+import java.time.temporal.ChronoUnit
 import java.util.Date
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -48,7 +63,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    DatePickerShow()
+                    MyCallendar()
                 }
             }
         }
@@ -56,41 +71,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-
-fun DatePickerShow(modifier: Modifier = Modifier) {
-    var isVisible by remember { mutableStateOf(false) }
+fun MyCallendar() {
     var selectedDate by remember { mutableStateOf<Long?>(null) }
-    Box (
+    var showDatePicker by remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
-    ){
-        Column (
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ){
-            Text(
-                text = if (selectedDate != null) {
-                    "Выбрана дата ${formatDate(selectedDate!!)}"
-                } else {
-                    "Дата не выбрана"
-                }
-            )
-            Spacer(Modifier.height(16.dp))
+    ) {
+        Column(
+        ) {
             Button(
-                onClick={
-                    isVisible = true
-                }
+                onClick = {
+                    showDatePicker = true
+                },
+                modifier = Modifier.padding(top = 50.dp).size(150.dp),
+                shape = CircleShape
             ) {
                 Text(
-                    text = "Выбрать дату"
+                    text = if (selectedDate != null) {
+                        "Изменить дату"
+                    } else {
+                        "Выбрать дату"
+                    },
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center
                 )
             }
-        }
-        if (isVisible) {
-            MyDatePicker(
-                onDateSelected = {dateMillis -> selectedDate = dateMillis},
-                initialDate = selectedDate,
-                onDismiss = {isVisible = false}
-            )
+            if (showDatePicker) {
+                MyDatePicker(
+                    onDismiss = {
+                        showDatePicker = false
+                    },
+                    onDataSelected = {
+                        millis -> selectedDate = millis
+                    },
+                    initialDate = selectedDate
+                )
+            }
+            if (selectedDate != null) {
+                Text(text = formatDate(selectedDate!!))
+            }
         }
     }
 }
@@ -98,9 +118,9 @@ fun DatePickerShow(modifier: Modifier = Modifier) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyDatePicker(
-    onDateSelected: (Long?) -> Unit,
-    onDismiss: () -> Unit,
-    initialDate: Long? = null
+    onDismiss : () -> Unit,
+    onDataSelected : (Long?) -> Unit,
+    initialDate : Long?
 ) {
     val datePickerState = rememberDatePickerState(
         initialSelectedDateMillis = initialDate
@@ -108,26 +128,35 @@ fun MyDatePicker(
     DatePickerDialog(
         onDismissRequest = onDismiss,
         confirmButton = {
-            TextButton(onClick = {
-                onDateSelected(datePickerState.selectedDateMillis)
-                onDismiss()
-            }) {
-                Text("OK")
+            TextButton(
+                onClick = {
+                    onDataSelected(datePickerState.selectedDateMillis)
+                    onDismiss()
+                }
+            ) {
+                Text(
+                    text = "OK"
+                )
             }
         },
         dismissButton = {
-            TextButton(onClick = onDismiss) {
+            TextButton(
+                onClick = onDismiss
+            ) {
                 Text("Cancel")
             }
         }
 
     ) {
-        DatePicker(state = datePickerState)
+        DatePicker(
+            state = datePickerState
+        )
     }
+
 }
 
-fun formatDate(millisDate : Long): String {
-    val date = Date(millisDate)
+fun formatDate(dateMillis : Long) : String {
+    val date = Date(dateMillis)
     val format = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
     return format.format(date)
 }
